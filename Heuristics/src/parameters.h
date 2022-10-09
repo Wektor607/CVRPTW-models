e8a8ac9ff690521f1afd46fcf71d0dd1d97200fc
Wektor607 <1069035F@mail.ru>
2022-10-09 10:10:06 +0300

    Small edits
    Добавлена функция деления на помаршруты.

--- Heuristics/src/parameters.h
+++ Heuristics/src/parameters.h
@@ -2,5 +2,5 @@
 
 #define speed 60.0 //km/h
 #define TimeToDist 3600.0 * speed //
-#define KmToSeconds (3600.0 / speed) // перевод км в секунды с учетом средней скорости
+#define KmToSeconds 3600.0 / speed // перевод км в секунды с учетом средней скорости
 
