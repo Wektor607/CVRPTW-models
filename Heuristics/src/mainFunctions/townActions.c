77dac9540b0576c5408d458f2d58f0e5d8663100
Wektor607 <42221181+Wektor607@users.noreply.github.com>
2022-11-26 01:11:44 +0300

    Add files via upload

--- Heuristics/src/mainFunctions/townActions.c
+++ Heuristics/src/mainFunctions/townActions.c
@@ -99,91 +99,101 @@ void printTwTownList(const twtown* towns, int counttowns) {
     printf("]");
 }
 
+void depoShift(int lenSub, twtown *sub)
+{
+    twtown *subcopy = (twtown *) calloc(lenSub, sizeof(twtown));
+    int start = 0;
+    for (int e = 0; e < lenSub; ++e)
+    {
+        if (sub[e].t.name == 0)
+        {
+            start = e;
+            break;
+        }
+    }
+
+    if(start != 0)
+    {
+        for (int i = 0; i < lenSub; ++i)
+        {
+            if (i < start)
+                subcopy[lenSub - start + i] = sub[i];
+            else
+                subcopy[i - start] = sub[i];
+        }
+
+        for(int i = 0; i < lenSub; ++i)
+        {
+            sub[i] = subcopy[i];
+        }
+    }
+
+    free(subcopy);
+}
+
 
 void reverseTownTw(twtown *sub, int i, int j)
 {
-    // int s = (j - i) / 2;
-    // for (int k = 1; k <= s; ++k)
-    // {
-    //     swapTw(&sub[i + k], &sub[j - k + 1]);
-    // }
-    int s = (j + i) / 2;
-    if((j + i) % 2 == 1) {
-        for(int k = 0; k < (j - i + 1) / 2; k++)
-        {
-            swapTw(&sub[s - k], &sub[s+1+k]);
-        }
-    } else {
-        for(int k = 0; k < (j - i) / 2; k++)
-        {
-            swapTw(&sub[s-1-k], &sub[s+1+k]);
-        }
+    int s = (j - i) / 2;
+    for (int k = 1; k <= s; ++k)
+    {
+        swapTw(&sub[i + k], &sub[j - k + 1]);
     }
 }
 
-int moveElemsTw(twtown *sub, int start1, int end1, int start2, int end2)
+void moveElemsTw(twtown *sub, int i, int j, int k)
 {
-    int difference = (end1 - start1 - (end2 - start2));
-
-    twtown tmp;
-
-    twtown *mtmp = (twtown*)malloc(abs(difference) * sizeof(twtown));
-    if(mtmp == NULL) 
+    twtown *part1 = calloc(sizeof(twtown), j - i);
+    twtown *part2 = calloc(sizeof(twtown), k - j);
+    for (int start = i + 1, idx = 0; start <= j; ++start, ++idx)
     {
-        return -1;
+        part1[idx] = sub[start];
     }
-    for(int i = 0; i < abs(difference); i++) 
+    
+    for (int start = j + 1, idx = 0; start <= k; ++start, ++idx)
     {
-        if(difference > 0) 
-        {
-            mtmp[i] = sub[end1 + 1 - difference + i];
-        } 
-        else if(difference < 0) 
-        {
-            mtmp[i] = sub[start2 + i];
-        }
+        part2[idx] = sub[start];
     }
 
-    if(difference > 0) 
+    for (int idx = i + 1, part_idx = 0; idx < i + k - j + 1; ++idx, ++part_idx)
     {
-
-        for(int i = 0; i < end2 - end1; i++) 
-        {
-            sub[end1 + 1 - difference + i] = sub[end1 + 1 + i];
-        }
-
-        for(int i = 0; i < end2 - start2 + 1; i++) 
-        {
-            tmp = sub[start1 + i];
-            sub[start1 + i] = sub[start2 + i - difference];
-            sub[start2 + i - difference] = tmp;
-        }
-
-        for(int i = 0; i < abs(difference); i++) 
-        {
-            sub[end2 + 1 - difference +i] = mtmp[i];
-        }
-
-    } 
-    else if(difference < 0) 
-    {
-
-        for(int i = 0; i < end1 - start1 + 1; i++) {
-            tmp = sub[start1 + i];
-            sub[start1 + i] = sub[start2 - difference + i];
-            sub[start2 - difference + i] = tmp;
-        }
-
-        for(int i = 0; i < start2-start1; i++) {
-            sub[start2 - difference - 1 - i] = sub[start2 - 1 - i];
-        }
-
-        for(int i = 0; i < abs(difference); i++) {
-            sub[start1 + i] = mtmp[i];
-        }
-
+        sub[idx] = part2[part_idx];
     }
 
-    free(mtmp);
-    return 0;
+    for (int idx = i + k - j + 1, part_idx = 0; idx <= k; ++idx, ++part_idx)
+    {
+        sub[idx] = part1[part_idx];
+    }
+
+    free(part1);
+    free(part2);
+}
+
+
+void reverse_segment_if_better(halfmatrix *m, twtown *tour, int i, int j, int k, int len)
+{
+    // Given tour [...A-B...C-D...E-F...]
+    int A = tour[i].t.name, B = tour[i+1].t.name, C = tour[j].t.name, D = tour[j+1].t.name, E = tour[k].t.name, F = tour[(k+1) % len].t.name;
+    double d0 = getByTown(m, A, B) + getByTown(m, C, D) + getByTown(m, E, F);
+    double d1 = getByTown(m, A, C) + getByTown(m, B, D) + getByTown(m, E, F);
+    double d2 = getByTown(m, A, B) + getByTown(m, C, E) + getByTown(m, D, F);
+    double d3 = getByTown(m, A, D) + getByTown(m, E, B) + getByTown(m, C, F);
+    double d4 = getByTown(m, F, B) + getByTown(m, C, D) + getByTown(m, E, A);
+    
+    if (d0 > d1)
+    {
+        reverseTownTw(tour, i, j);
+    }
+    else if (d0 > d2)
+    {
+        reverseTownTw(tour, j, k);
+    }
+    else if (d0 > d4)
+    {
+        reverseTownTw(tour, i, k);
+    }
+    else if (d0 > d3)
+    {
+        moveElemsTw(tour, i, j, k);
+    }
 }
